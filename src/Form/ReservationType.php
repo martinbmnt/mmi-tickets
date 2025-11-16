5f73016581a9c1f74ea93b4cc240e47ca5fcf8af
Martin Beaumont <hello@martinbeaumont.dev>
2025-11-16 11:06:56 +0100

    Conditionnal fields on reservation form

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -8,22 +8,34 @@ use App\Enum\ReservationStatus;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('status', EnumType::class, [
-                'class' => ReservationStatus::class,
-            ])
-            ->add('pseudo')
-            ->add('concert', EntityType::class, [
-                'class' => Concert::class,
-                'choice_label' => 'musicGroup',
-            ]);
+        $builder->add('pseudo', TextType::class);
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+
+            if ($form->getConfig()->getMethod() === 'PUT') {
+                // Update form
+                $form->add('status', EnumType::class, [
+                    'class' => ReservationStatus::class,
+                ]);
+            } else {
+                // Create form
+                $form->add('concert', EntityType::class, [
+                    'class' => Concert::class,
+                    'choice_label' => 'musicGroup',
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
